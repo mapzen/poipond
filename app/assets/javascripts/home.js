4e197f46499969ba3f6d20cdbc86415872b27e36
Randy Meech <randy.meech@gmail.com>
2014-02-01 13:00:17 -0500

    edit list

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -9,9 +9,9 @@ function checkIn() {
         if (geoJson.hasOwnProperty(key)) {
           obj = geoJson[key];
           $('#poi-results-list').append([
-            '<span><a href="/pois/' + obj.id + '" class="list-group-item">' + obj.name,
-            '<br/>' + obj.full_addr + '<br/>' + obj.distance + ' miles</a></span>',
-            '<span class="pull-right"><img src="/assets/edit-icon.png" width="20" height="20"></span>'
+            '<a href="/pois/' + obj.id + '" class="list-group-item">' + obj.name,
+            '<br/>' + obj.full_addr + '<br/>' + obj.distance + ' miles',
+            '<img src="/assets/edit-icon.png" width="20" height="20" class="pull-right"></a>'
           ].join(''))
         }
       }
