8a43666d9a2a44fca06781c744e446f1aac5edcd
Randy Meech <randy.meech@gmail.com>
2014-02-02 11:19:13 -0500

    cast to float

--- app/controllers/api/v0/pois_controller.rb
+++ app/controllers/api/v0/pois_controller.rb
@@ -1,7 +1,7 @@
 class Api::V0::PoisController < Api::V0Controller
 
   def closest
-    @pois = Poi.closest(params[:lat], params[:lon])
+    @pois = Poi.closest(params[:lat].to_f, params[:lon].to_f)
   end
 
 end
