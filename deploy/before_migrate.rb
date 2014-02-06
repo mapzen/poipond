97da59a8b11b45fba7b4730dc47a2f27fa993ffa
Grant Heffernan <heffergm@gmail.com>
2014-02-06 10:15:19 -0500

    wrong link: elasticsearch.yml => shards.yml

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -1,6 +1,6 @@
 Chef::Log.info("Running deploy/before_migrate.rb...")
 
-cfgs = %w(elasticsearch.yml redis.yml).each do |cfg|
+cfgs = %w(shards.yml redis.yml).each do |cfg|
   Chef::Log.info("Symlinking #{release_path}/config/#{cfg} to /etc/mapzen_poipond/#{cfg}")
   link "#{release_path}/config/#{cfg}" do
     to "/etc/mapzen_poipond/#{cfg}"
