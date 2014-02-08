f913038a6d747fc80fb232c1e9aff79209f488ee
Randy Meech <randy.meech@gmail.com>
2014-02-08 06:38:19 -0500

    cors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,21 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_filter :set_cors_headers
+
   def current_user
     return unless session[:user_id]
     @current_user ||= User.find(session[:user_id])
   end
   helper_method :current_user
+
+  private
+
+  def set_cors_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'GET'
+    headers['Access-Control-Request-Method'] = 'GET'
+    headers['Access-Control-Allow-Headers'] = '*'
+  end
 
 end
