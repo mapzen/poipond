9aa4834e4b667e1cdc9100d9d4098360c1514f3c
Grant Heffernan <heffergm@gmail.com>
2014-02-10 11:24:56 -0500

    hardcode deploy hook

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -1,13 +1,13 @@
 Chef::Log.info("Running deploy/before_migrate.rb...")
 
 node[:mapzen_poipond][:custom_cfgs].each do |cfg|
-  template "#{deploy_to}/shared/config" do
+  template "/srv/www/poipond/shared/config" do
     source "#{cfg}.erb"
     mode 0644
   end
 
   link "#{release_path}/config/#{cfg}" do
-    to "#{deploy_to}/shared/config/#{cfg}"
+    to "/srv/www/poipond/shared/config/#{cfg}"
   end
 end
 
