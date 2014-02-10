b486382df23ff2f1722503f9ff889654bd21dad4
Grant Heffernan <heffergm@gmail.com>
2014-02-10 11:26:54 -0500

    Revert "deploy hooks"
    This reverts commit 3b9499d400374dd6b7a641a087730230424bfcab.

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -1,13 +1,13 @@
 Chef::Log.info("Running deploy/before_migrate.rb...")
 
 node[:mapzen_poipond][:custom_cfgs].each do |cfg|
-  template "#{deploy_to}/shared/config" do
+  template "#{shared_path}/config" do
     source "#{cfg}.erb"
     mode 0644
   end
 
   link "#{release_path}/config/#{cfg}" do
-    to "#{deploy_to}/shared/config/#{cfg}"
+    to "#{shared_path}/config/#{cfg}"
   end
 end
 
