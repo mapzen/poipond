a55765ba8c636799d267719f77776d8b460c8a51
Grant Heffernan <heffergm@gmail.com>
2014-02-10 11:39:15 -0500

    fix link in deploy hook

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -5,7 +5,6 @@ Chef::Log.info("Running deploy/before_migrate.rb...")
 
    link "#{release_path}/config/#{cfg}" do
      to "#{node[:mapzen_poipond][:cfg_dir]}/#{cfg}"
-     to "#{shared_path}/config/#{cfg}"
    end
  end
 
