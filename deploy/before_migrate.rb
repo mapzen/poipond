3b9499d400374dd6b7a641a087730230424bfcab
Grant Heffernan <heffergm@gmail.com>
2014-02-10 11:22:38 -0500

    deploy hooks

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -1,13 +1,13 @@
 Chef::Log.info("Running deploy/before_migrate.rb...")
 
 node[:mapzen_poipond][:custom_cfgs].each do |cfg|
-  template "#{shared_path}/config" do
+  template "#{deploy_to}/shared/config" do
     source "#{cfg}.erb"
     mode 0644
   end
 
   link "#{release_path}/config/#{cfg}" do
-    to "#{shared_path}/config/#{cfg}"
+    to "#{deploy_to}/shared/config/#{cfg}"
   end
 end
 
