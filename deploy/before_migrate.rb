071a65c0adec5a60f74018bb2bba82edb0286152
Grant Heffernan <heffergm@gmail.com>
2014-02-10 11:21:56 -0500

    Revert "Revert "change before_migrate""
    This reverts commit c60c29848ecaec65fec67d3f66b66a184051b14f.

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -1,9 +1,13 @@
 Chef::Log.info("Running deploy/before_migrate.rb...")
 
 node[:mapzen_poipond][:custom_cfgs].each do |cfg|
-  Chef::Log.info("Symlinking #{release_path}/config/#{cfg} to #{node[:mapzen_poipond][:cfg_dir]}/#{cfg}")
+  template "#{shared_path}/config" do
+    source "#{cfg}.erb"
+    mode 0644
+  end
+
   link "#{release_path}/config/#{cfg}" do
-    to "#{node[:mapzen_poipond][:cfg_dir]}/#{cfg}"
+    to "#{shared_path}/config/#{cfg}"
   end
 end
 
