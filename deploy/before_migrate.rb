1352e78cc144841ce57e645ce83296a9af064d5b
Grant Heffernan <heffergm@gmail.com>
2014-02-10 11:37:25 -0500

    before migrate hook... maybe this time... maybe

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -1,13 +1,11 @@
 Chef::Log.info("Running deploy/before_migrate.rb...")
 
-node[:mapzen_poipond][:custom_cfgs].each do |cfg|
-  template "#{shared_path}/config" do
-    source "#{cfg}.erb"
-    mode 0644
-  end
+ node[:mapzen_poipond][:custom_cfgs].each do |cfg|
+  Chef::Log.info("Symlinking #{release_path}/config/#{cfg} to #{node[:mapzen_poipond][:cfg_dir]}/#{cfg}")
 
-  link "#{release_path}/config/#{cfg}" do
-    to "#{shared_path}/config/#{cfg}"
-  end
-end
+   link "#{release_path}/config/#{cfg}" do
+     to "#{node[:mapzen_poipond][:cfg_dir]}/#{cfg}"
+     to "#{shared_path}/config/#{cfg}"
+   end
+ end
 
