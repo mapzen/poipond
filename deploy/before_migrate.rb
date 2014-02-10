51762ed6442694a3db692188801dfddd84553c7f
Grant Heffernan <heffergm@gmail.com>
2014-02-10 11:26:46 -0500

    Revert "hardcode deploy hook"
    This reverts commit 9aa4834e4b667e1cdc9100d9d4098360c1514f3c.

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -1,13 +1,13 @@
 Chef::Log.info("Running deploy/before_migrate.rb...")
 
 node[:mapzen_poipond][:custom_cfgs].each do |cfg|
-  template "/srv/www/poipond/shared/config" do
+  template "#{deploy_to}/shared/config" do
     source "#{cfg}.erb"
     mode 0644
   end
 
   link "#{release_path}/config/#{cfg}" do
-    to "/srv/www/poipond/shared/config/#{cfg}"
+    to "#{deploy_to}/shared/config/#{cfg}"
   end
 end
 
