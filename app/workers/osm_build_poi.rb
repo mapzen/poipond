cc91aa686dd4c2728ea1af7447f424e943692b81
Randy Meech <randy.meech@gmail.com>
2014-02-16 18:42:27 -0500

    update records correctly

--- app/workers/osm_build_poi.rb
+++ app/workers/osm_build_poi.rb
@@ -5,20 +5,22 @@ class OsmBuildPoi
   def perform(osm_type, osm_poi, category_id)
     category = Category.find(category_id)
     latlon = JSON.parse(osm_poi['st_asgeojson'])
-    poi = Poi.where(osm_id: osm_poi['osm_id'], osm_type: osm_type).first_or_create! do |p|
-      p.name = osm_poi['name']
-      p.name = category.name if p.name.blank?
-      p.addr_housenumber = osm_poi['addr:housenumber']
-      p.addr_street = osm_poi['addr:street']
-      p.addr_city = osm_poi['addr:city']
-      p.addr_postcode = osm_poi['addr:postcode']
-      p.phone = osm_poi['phone']
-      p.website = osm_poi['website']
-      p.lat = latlon['coordinates'][1]
-      p.lon = latlon['coordinates'][0]
-    end
+    poi = Poi.where(osm_id: osm_poi['osm_id'], osm_type: osm_type).first
+    poi = Poi.new(osm_id: osm_poi['osm_id'], osm_type: osm_type) if poi.nil?
+    name = osm_poi['name']
+    name = category.name if name.blank?
+    poi.update_attributes!(
+      :name => name,
+      :addr_housenumber => osm_poi['addr:housenumber'],
+      :addr_street => osm_poi['addr:street'],
+      :addr_city => osm_poi['addr:city'],
+      :addr_postcode => osm_poi['addr:postcode'],
+      :phone => osm_poi['phone'],
+      :website => osm_poi['website'],
+      :lat => latlon['coordinates'][1],
+      :lon => latlon['coordinates'][0]
+    )
     poi.categories << category unless poi.categories.include? category
-    poi.save! if poi.changed?
   end
 
 end
