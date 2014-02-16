a213f485ce0ce19a2534c779488f672acdb32867
Randy Meech <randy.meech@gmail.com>
2014-02-16 08:42:09 -0500

    deleted pois

--- app/workers/osm_download.rb
+++ app/workers/osm_download.rb
@@ -10,21 +10,25 @@ class OsmDownload
     return if poi.osm_id.to_i < 0
     url = "#{OSM_API_BASE}/#{poi.osm_type}/#{poi.osm_id}"
     response = user.osm_access_token.get(url)
-    hash = XmlSimple.xml_in(response.body)[poi.osm_type].first.symbolize_keys
-    tags = Poi.decode_tags(hash[:tag])
-    poi.update_attributes(
-      :lat => (hash[:lat] || poi.lat),
-      :lon => (hash[:lon] || poi.lon),
-      :version => hash[:version],
-      :tags => tags,
-      :name => tags[:name],
-      :addr_housenumber => tags[:addr_housenumber],
-      :addr_street => tags[:addr_street],
-      :addr_city => tags[:addr_city],
-      :addr_postcode => tags[:addr_postcode],
-      :phone => tags[:phone],
-      :website => tags[:website]
-    )
+    if response.body.blank?
+      poi.update_attributes(:osm_id=>"-#{poi.osm_id}")
+    else
+      hash = XmlSimple.xml_in(response.body)[poi.osm_type].first.symbolize_keys
+      tags = Poi.decode_tags(hash[:tag])
+      poi.update_attributes(
+        :lat => (hash[:lat] || poi.lat),
+        :lon => (hash[:lon] || poi.lon),
+        :version => hash[:version],
+        :tags => tags,
+        :name => tags[:name],
+        :addr_housenumber => tags[:addr_housenumber],
+        :addr_street => tags[:addr_street],
+        :addr_city => tags[:addr_city],
+        :addr_postcode => tags[:addr_postcode],
+        :phone => tags[:phone],
+        :website => tags[:website]
+      )
+    end
   end
 
 end
