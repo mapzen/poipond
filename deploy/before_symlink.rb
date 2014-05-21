fc69aa56ab02235715fdecfdcf44518c1c3c92bc
Grant Heffernan <heffergm@gmail.com>
2014-05-21 13:54:56 -0400

    deploy

--- deploy/before_symlink.rb
+++ deploy/before_symlink.rb
@@ -2,10 +2,17 @@ Chef::Log.info("Running deploy/before_symlink.rb...")
 
 rails_env = new_resource.environment["RAILS_ENV"]
 
+node[:mapzen_poipond][:custom_cfgs].each do |cfg|
+ Chef::Log.info("Symlinking #{release_path}/config/#{cfg} to #{node[:mapzen_poipond][:cfg_dir]}/#{cfg}")
+
+  link "#{release_path}/config/#{cfg}" do
+    to "#{node[:mapzen_poipond][:cfg_dir]}/#{cfg}"
+  end
+end
+
 Chef::Log.info("Precompiling assets for RAILS_ENV=#{rails_env}...")
 execute "rake assets:precompile" do
   cwd release_path
   command "bundle exec rake assets:precompile"
   environment "RAILS_ENV" => rails_env
 end
-
