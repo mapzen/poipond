c3eb284ca18efbcbd5917b718e3114209c2e31d9
Grant Heffernan <heffergm@gmail.com>
2015-02-08 16:46:08 -0500

    push configs into place before_symlink and before_migrate

--- deploy/before_symlink.rb
+++ deploy/before_symlink.rb
@@ -1,5 +1,23 @@
 Chef::Log.info("Running deploy/before_symlink.rb...")
 
+# move configs into place
+node[:mapzen_poipond][:custom_cfgs].each do |cfg|
+ Chef::Log.info("Symlinking #{release_path}/config/#{cfg} to #{node[:mapzen_poipond][:cfg_dir]}/#{cfg}")
+
+  link "#{release_path}/config/#{cfg}" do
+    to "#{node[:mapzen_poipond][:cfg_dir]}/#{cfg}"
+  end
+end
+
+template "#{node[:deploy][:poipond][:deploy_to]}/shared/config/database.yml" do
+  cookbook  'mapzen_poipond'
+  source    'database.yml.erb'
+  owner     'www-data'
+  mode      0644
+  only_if { node[:hostname] =~ /^rails-app/ }
+end
+
+# precompile assets
 rails_env = new_resource.environment["RAILS_ENV"]
 
 Chef::Log.info("Precompiling assets for RAILS_ENV=#{rails_env}...")
