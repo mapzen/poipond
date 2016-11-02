946dedd4ff63e686611499c63089fdf79341a127
souperneon <ekta.daryanani@gmail.com>
2016-11-02 10:55:15 -0400

    removed mapbox map and added bubble wrap using mapzenjs

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -22,8 +22,8 @@ function checkIn() {
 }
 
 $(document).ready(function() {
-  map = L.mapbox.map('map', 'randyme.h29f04e6', { zoomControl: false });
-  map.locate({ setView: true, maxZoom: 16, watch: true, enableHighAccuracy: true });
+  map = L.Mapzen.map('map');
+  map.setView([37.7749, -122.4194], 12);
   map.on('locationfound', function (e) {
     var radius = e.accuracy / 2;
     for (i=0; i<markers.length; i++) {
